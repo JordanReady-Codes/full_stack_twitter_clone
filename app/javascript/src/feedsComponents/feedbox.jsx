a0e78197147e5aded906b53db7b195e684fcccbf
JordanReady <88412345+JordanReady@users.noreply.github.com>
2022-10-23 18:11:45 +0000

    update to feeds components

--- app/javascript/src/feedsComponents/feedbox.jsx
+++ app/javascript/src/feedsComponents/feedbox.jsx
@@ -1,24 +1,58 @@
 import React from 'react'
 import Tweet from './tweet'
 import Tweetbox from './tweetbox'
+import { safeCredentials, handleErrors } from '../utils/fetchHelper'
 
 
-function Feedbox(props) {
-  return (
-    <React.Fragment>
+class Feedbox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      message: '',
+      id: '',
+      tweets: []
+    };
+  }
+
+  componentDidMount() {
+    this.getTweets();
+  }
+
+  getTweets = () => {
+    fetch('/api/tweets', safeCredentials({
+      method: 'GET',
+    }))
+    .then(handleErrors)
+    .then(data => {
+      this.setState({
+        tweets: data.tweets,
+        message: data.message,
+        username: data.username,
+        id: data.id,
+      });
+      console.log('data', data)
+    })
+  }
+
+  render() {
+    const { tweets, username, message, id } = this.state;
+
+    return (
+      <React.Fragment>
         <div className="col-9 feed-box border border-primary rounded shadow">
-            <Tweetbox />
-            <div className="feed mt-5">
-                <Tweet />
-                <Tweet />
-                <Tweet />
-                <Tweet />
-            </div>
-          </div>
-    </React.Fragment>
-  )
+        <Tweetbox />
+          {tweets.map(() => {
+            return (
+              <div className="feedbox mt-4">
+              <Tweet id={id} message={message} username={username} />
+              </div>
+            )
+          })}
+        </div>
+      </React.Fragment>
+    )
+  }
 }
-
-
 
 export default Feedbox
