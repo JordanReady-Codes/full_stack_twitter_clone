248f37c5c7dd4e303dbed06a3442c7ee335b1682
JordanReady <88412345+JordanReady@users.noreply.github.com>
2022-11-04 04:08:32 +0000

    missing manifest

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,15 +1,3 @@
 const { environment } = require('@rails/webpacker')
 
-const path = require('path')
-
-const customConfig = {
-  resolve: {
-    alias: {
-      '@src': path.resolve(__dirname, '..', '..', 'app/javascript/src'),
-    }
-  }
-}
-
-environment.config.merge(customConfig);
-
 module.exports = environment
