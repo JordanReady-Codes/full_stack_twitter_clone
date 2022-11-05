a1bbb1f6688ec0d47d2296908012778c25aaeb02
JordanReady <88412345+JordanReady@users.noreply.github.com>
2022-11-05 21:45:40 +0000

    new route for user page

--- app/javascript/src/userFeedComponents/userFeed.jsx
+++ app/javascript/src/userFeedComponents/userFeed.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import Navbar from "../feedsComponents/navbar";
+import ProfileCard from "../feedsComponents/profileCard";
+import Feedbox from "../feedsComponents/feedbox";
+
 
 const UserFeed = () => {
   return (
-    <div>
-      <h1>User Feed</h1>
-    </div>
-  );
+    <React.Fragment>
+      <Navbar />
+      <div className="main container">
+        <div className="row">
+          <div className="profile-trends col-3">
+            <ProfileCard />
+          </div>
+          <Feedbox />
+        </div>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default UserFeed;
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <UserFeed />,
+    document.body.appendChild(document.createElement("div"))
+  )
+})
+
+
